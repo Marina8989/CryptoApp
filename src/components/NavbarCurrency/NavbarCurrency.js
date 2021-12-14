ab7f8639f43a0cc4f6940f150d270fabd53cbdef
Marina8989 <marina.mill209@gmail.com>
2021-12-14 14:26:31 -0800

    currency page

--- src/components/NavbarCurrency/NavbarCurrency.js
+++ src/components/NavbarCurrency/NavbarCurrency.js
@@ -6,13 +6,9 @@ import {
   StyledButton,
 } from "./NavbarCurrency.styles";
 
-function NavbarCurrency(props){
+function NavbarCurrency(props) {
   const [isVisible, setIsVisible] = useState(false);
   const [currencyList, setCurrencyList] = useState(props.currencyList);
-  // state = {
-  //   isVisible: false,
-  //   currencyList: this.props.currencyList,
-  // };
 
   const handleClick = () => {
     setIsVisible(true);
@@ -22,96 +18,23 @@ function NavbarCurrency(props){
     setIsVisible(false);
     props.handleCoinCurrency(el);
   };
-    return (
-      <StyledMenuCurrency>
-        <StyledMenuCurrencyH4 onClick={handleClick}>
-          {props.currencyDefault}
-          <MdArrowDropDown className="menu-icon-arrow" />
-        </StyledMenuCurrencyH4>
-        {isVisible && (
-          <>
-            {currencyList.map((item, index) => (
-              <StyledButton
-                onClick={() => handleCoinCurrency(item)}
-                key={index}
-              >
-                {item.toUpperCase()}
-              </StyledButton>
-            ))}
-          </>
-        )}
-      </StyledMenuCurrency>
-    );
-  }
-
+  return (
+    <StyledMenuCurrency>
+      <StyledMenuCurrencyH4 onClick={handleClick}>
+        {props.currencyDefault}
+        <MdArrowDropDown className="menu-icon-arrow" />
+      </StyledMenuCurrencyH4>
+      {isVisible && (
+        <>
+          {currencyList.map((item, index) => (
+            <StyledButton onClick={() => handleCoinCurrency(item)} key={index}>
+              {item.toUpperCase()}
+            </StyledButton>
+          ))}
+        </>
+      )}
+    </StyledMenuCurrency>
+  );
+}
 
 export default NavbarCurrency;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from "react";
-// import { MdArrowDropDown } from "react-icons/md";
-// import {
-//   StyledMenuCurrency,
-//   StyledMenuCurrencyH4,
-//   StyledButton,
-// } from "./NavbarCurrency.styles";
-
-// class NavbarCurrency extends React.Component {
-//   state = {
-//     isVisible: false,
-//     currencyList: this.props.currencyList,
-//   };
-
-//   handleClick = () => {
-//     this.setState({ isVisible: true });
-//   };
-
-//   handleCoinCurrency = (el) => {
-//     this.setState({ isVisible: false });
-//     this.props.handleCoinCurrency(el);
-//   };
-//   render() {
-//     return (
-//       <StyledMenuCurrency>
-//         <StyledMenuCurrencyH4 onClick={this.handleClick}>
-//           {this.props.currencyDefault}
-//           <MdArrowDropDown className="menu-icon-arrow" />
-//         </StyledMenuCurrencyH4>
-//         {this.state.isVisible && (
-//           <>
-//             {this.state.currencyList.map((item, index) => (
-//               <StyledButton
-//                 onClick={() => this.handleCoinCurrency(item)}
-//                 key={index}
-//               >
-//                 {item.toUpperCase()}
-//               </StyledButton>
-//             ))}
-//           </>
-//         )}
-//       </StyledMenuCurrency>
-//     );
-//   }
-// }
-
-// export default NavbarCurrency;
