e3f1c23f70f7ba4e8f196506b94ad44b8079cea2
Marina8989 <marina.mill209@gmail.com>
2021-12-14 17:51:07 -0800

    reack into hooks

--- src/components/NavbarHeader/NavbarHeader.js
+++ src/components/NavbarHeader/NavbarHeader.js
@@ -20,30 +20,3 @@ function NavbarHeader() {
 }
 
 export default NavbarHeader;
-
-// import React from "react";
-// import Navbar from "./components/Navbar/Navbar";
-// import { FaAdjust } from "react-icons/fa";
-// import { StyledDiv } from "./NavbarHeader.styles";
-
-// class NavbarHeader extends React.Component {
-//   state = {
-//     on: false,
-//   };
-
-//   handleSwitch = () => {
-//     this.setState({ on: !this.state.on });
-//   };
-//   render() {
-//     return (
-//       <StyledDiv>
-//         <StyledDiv className="navbar">
-//           <Navbar />
-//           <FaAdjust onClick={this.handleSwitch} className="menu-icon" />
-//         </StyledDiv>
-//       </StyledDiv>
-//     );
-//   }
-// }
-
-// export default NavbarHeader;
