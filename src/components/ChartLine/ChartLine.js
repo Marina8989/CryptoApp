78cbd5a602895f48c74a6627ba010b8740c02dd9
Marina8989 <marina.mill209@gmail.com>
2021-12-17 14:02:33 -0800

    chart line into hooks

--- src/components/ChartLine/ChartLine.js
+++ src/components/ChartLine/ChartLine.js
@@ -1,74 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
 import axios from "axios";
 import ChartLineLegend from "../ChartLineLegend/ChartLineLegend.js";
 
-class ChartLine extends React.Component {
-  state = {
-    chartMarket: [],
-    chartLabel: [],
-  };
+function ChartLine(props) {
+  const [chartMarket, setChartMarket] = useState([]);
+  const [chartLabel, setChartLabel] = useState([]);
 
-  getChartInfo = async (coinCurrency) => {
+  const getChartInfo = async (coinCurrency) => {
     const { data } = await axios(
       `https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=${coinCurrency}&days=30&interval=daily`
     );
     const chartMarket = data.prices.map((el) => el[1]);
     const chartLabel = data.prices.map((el) => new Date(el[0]).getDate());
-    this.setState({ chartMarket, chartLabel });
+    setChartMarket(chartMarket);
+    setChartLabel(chartLabel);
   };
-  componentDidMount() {
-    this.getChartInfo(this.props.currencyDefault.toLowerCase());
-  }
-  render() {
-    return (
-      <div className="chart-line">
-        <ChartLineLegend />
-        <Line
-          data={{
-            labels: this.state.chartLabel,
-            datasets: [
-              {
-                label: this.props.currencyDefault,
-                data: this.state.chartMarket,
-                fill: true,
-                backgroundColor: "rgba(23, 82, 34, .2)",
-                borderColor: "rgb(1,226,37)",
-                tension: 0.6,
+  useEffect(() => {
+    getChartInfo(props.currencyDefault.toLowerCase());
+  }, []);
+  return (
+    <div className="chart-line">
+      <ChartLineLegend />
+      <Line
+        data={{
+          labels: chartLabel,
+          datasets: [
+            {
+              label: props.currencyDefault,
+              data: chartMarket,
+              fill: true,
+              backgroundColor: "rgba(23, 82, 34, .2)",
+              borderColor: "rgb(1,226,37)",
+              tension: 0.6,
+            },
+          ],
+        }}
+        options={{
+          plugins: {
+            legend: {
+              display: false,
+            },
+          },
+          scales: {
+            y: {
+              grid: {
+                display: false,
+                drawTicks: false,
+                borderWidth: 0,
               },
-            ],
-          }}
-          options={{
-            plugins: {
-              legend: {
+              ticks: {
                 display: false,
               },
             },
-            scales: {
-              y: {
-                grid: {
-                  display: false,
-                  drawTicks: false,
-                  borderWidth: 0,
-                },
-                ticks: {
-                  display: false,
-                },
-              },
-              x: {
-                grid: {
-                  display: false,
-                  borderWidth: 0,
-                },
+            x: {
+              grid: {
+                display: false,
+                borderWidth: 0,
               },
             },
-          }}
-          height={250}
-          width={415}
-        />
-      </div>
-    );
-  }
+          },
+        }}
+        height={250}
+        width={415}
+      />
+    </div>
+  );
 }
 
 export default ChartLine;
