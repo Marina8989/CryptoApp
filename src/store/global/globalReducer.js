95271a7d922c5ccc2249d3ab59553f5a35aafa5d
Marina8989 <marina.mill209@gmail.com>
2022-01-13 10:32:59 -0800

    global to redux

---
+++ src/store/global/globalReducer.js
@@ -0,0 +1,25 @@
+const initialState = {
+  globalList: [],
+};
+
+export const GET_GLOBAL_SUCCESS = "GET_GLOBAL_SUCCESS";
+export const GET_GLOBAL_ERROR = "GET_GLOBAL_ERROR";
+
+function globalReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_GLOBAL_SUCCESS:
+      return {
+        ...state,
+        globalList: [...state.globalList, action.payload],
+      };
+    case GET_GLOBAL_ERROR:
+      return {
+        ...state,
+        globalList: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+export default globalReducer;
